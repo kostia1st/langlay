83b4913fc888bdf2579b12a7f451041291465005
estorski <test9@mormat.name>
2016-04-30 22:24:16 +0300

    Removing unused usings

--- Langlay.App/Services/Hotkey/IHotkeyService.cs
+++ Langlay.App/Services/Hotkey/IHotkeyService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Product
+﻿namespace Product
 {
     public interface IHotkeyService
     {
--- Langlay.Common/AppSpecific.cs
+++ Langlay.Common/AppSpecific.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Product.Common
+﻿namespace Product.Common
 {
     public static class AppSpecific
     {
